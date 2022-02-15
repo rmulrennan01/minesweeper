87cdea07b64a3f18dc26e8143270635acdcb8430
rmulrennan01 <rpmulrennan@gmail.com>
2022-02-15 12:32:49 -0600

    Added functions for randomized mine placement

--- src/Game.js
+++ src/Game.js
@@ -8,29 +8,37 @@ function Game() {
     const [mine_coord, set_mine_coord] = useState([]); 
     
 
+    
+    const populateTable = () =>{
+        set_tile_data([]); 
+        buildTable(); 
+    }
+
+    
     const buildTable = () =>{
-        let tempData = []; 
-        let minesAdded = 0; 
-        generateMineLocations(); 
+        let tempData = [];        
         
         for (let i = 0; i<board_size; i++){
             let tempRow = []; 
             for (let k = 0; k<board_size; k++){
-                if(mine_coord.includes([i,k])){
-                    tempRow.push(["X"]); 
-                }
-                else{
                     tempRow.push([0]); 
-                }
             }
             tempData.push(tempRow); 
         }
-        set_tile_data(tempData); 
+        //set_tile_data(tempData); 
+
+        generateMineLocations(); 
+        
+        set_tile_data(placeMines(tempData));
+
     }
 
+
     const generateMineLocations = () =>{
+       
         let temp_coords = []; 
         for (let i = 0; i<mine_count; i++){
+      
             let tempX = Math.floor(Math.random()*(mine_count)); 
             let tempY= Math.floor(Math.random()*(mine_count)); 
             while(mine_coord.includes([tempX,tempY])){
@@ -38,20 +46,24 @@ function Game() {
                 tempY= Math.floor(Math.random()*(mine_count)); 
             }
             temp_coords.push([tempX, tempY]); 
-
         }
-        set_mine_coord(temp_coords); 
         
+       set_mine_coord(temp_coords); 
     }
 
-
-
-
-    
+    const placeMines = (table) =>{
+       // console.log(tile_data); 
+        let tempBoard = table; 
+        mine_coord.map(function(item){
+            tempBoard[item[0]][item[1]]="M";
+        } )
+        return(tempBoard); 
+       // set_tile_data(tempBoard); 
+    }
 
   return (
     <div> 
-        <button onClick={buildTable}> Generate</button>
+        <button onClick={()=>populateTable()}> Generate</button>
         <Grid data={tile_data}/>
     </div> 
   )
--- src/Grid.js
+++ src/Grid.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./Grid.css"
 
 function Grid(props) {
+  
 
     const renderBoard = (row) => {
         return(
